577d77f94c42e93c0b71f1abd0bbe6c2baf96501
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2023-12-02 00:21:43 +0100

    build: added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+
   <router-view />
 </template>
 
